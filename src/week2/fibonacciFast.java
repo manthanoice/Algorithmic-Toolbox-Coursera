1dd3ea1347f636d9157b66a82c8b9b087d2dff6f
Manthanoice <mvbosamiya20@gmail.com>
2021-09-11 15:21:12 +0530

    change

--- src/week2/fibonacciFast.java
+++ src/week2/fibonacciFast.java
@@ -3,17 +3,17 @@ package week2;
 import java.util.Scanner;
 
 public class fibonacciFast {
-    static long fibonacci(int n){
-        long[] arr = new long[n];
+    static long fibonacci(long n){
+        long[] arr = new long[(int)n];
         arr[0]=0;
         arr[1]=1;
         for(int i=2; i<n; i++)
             arr[i]=arr[i-1]+arr[i-2];
-        return arr[n-1];
+        return arr[(int)n-1];
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         System.out.println(fibonacci(n));
     }
 }
