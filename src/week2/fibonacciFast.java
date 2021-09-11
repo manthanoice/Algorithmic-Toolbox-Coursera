b4e571a13f62c9ed45448f8016cda3d544600840
Manthanoice <mvbosamiya20@gmail.com>
2021-09-11 15:26:58 +0530

    change

--- src/week2/fibonacciFast.java
+++ src/week2/fibonacciFast.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class fibonacciFast {
     static long fibonacci(long n){
-        long[] arr = new long[(int)n];
+        if(n<=1)
+            return n;
+        long[] arr = new long[(int)n+1];
         arr[0]=0;
         arr[1]=1;
-        for(int i=2; i<n; i++)
+        for(int i=2; i<n+1; i++)
             arr[i]=arr[i-1]+arr[i-2];
-        return arr[(int)n-1];
+        return arr[(int)n];
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
