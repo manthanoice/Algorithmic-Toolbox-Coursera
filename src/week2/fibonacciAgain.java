104a002daf5571f187265e106e0a70a23cd7168f
Manthanoice <mvbosamiya20@gmail.com>
2021-09-13 17:48:44 +0530

    add huge

---
+++ src/week2/fibonacciAgain.java
@@ -0,0 +1,45 @@
+package week2;
+
+//https://www.youtube.com/watch?v=Nu-lW-Ifyec
+//http://webspace.ship.edu/msrenault/fibonacci/fiblist.htm
+
+//There's a pattern called Pisano pattern more like Fibonacci pattern, look up those links for better understanding!
+
+import java.util.Scanner;
+
+public class fibonacciAgain {
+    static long pisanoPattern(long m) {
+        long a = 0;
+        long b = 1;
+        long c;
+        long count = 0;
+        for (int i = 0; i<m*m; i++) {
+            c = (a + b) % m;
+            a = b;
+            b = c;
+            if (a == 0 && b == 1) {
+                count = i + 1;
+                break;
+            }
+        }
+        return count;
+    }
+    static long hugeFibonacci(long n, long m) {
+        long remainder = n % pisanoPattern(m);
+        long a = 0;
+        long b = 1;
+        long c = remainder;
+        for (int i = 1; i<remainder; i++) {
+            c = (a + b) % m;
+            a = b;
+            b = c;
+        }
+        return c % m;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long x = sc.nextLong();
+        System.out.println(hugeFibonacci(n,x));
+    }
+}
