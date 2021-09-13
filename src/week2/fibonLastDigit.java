4ff4f034c9bfff00aafa2c7cfd4717c3943d35ac
Manthanoice <mvbosamiya20@gmail.com>
2021-09-13 16:54:31 +0530

    add last digit

---
+++ src/week2/fibonLastDigit.java
@@ -0,0 +1,21 @@
+package week2;
+
+import java.util.Scanner;
+
+public class fibonLastDigit {
+    static long fibonacci(int n){
+        if(n<=1)
+            return n;
+        long []arr = new long[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i<n+1; i++)
+            arr[i]=(arr[i-1]+arr[i-2])%10;
+        return arr[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+}
