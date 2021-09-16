5b2c3f0dbe9203d9d6bc168ab01d4c91ab306b98
Manthanoice <mvbosamiya20@gmail.com>
2021-09-16 17:06:45 +0530

    add car fueling

---
+++ src/week3/carFueling.java
@@ -0,0 +1,38 @@
+package week3;
+
+import java.util.Scanner;
+
+public class carFueling {
+    static int computeMinRefills(int dist, int tank, int[] stops) {
+        int currentRefill = 0;
+        int numRefill = 0;
+        int[] allStop = new int[stops.length + 2];
+        System.arraycopy(stops, 0, allStop, 1, stops.length);
+        allStop[stops.length + 1] = dist;
+        if (tank > dist)
+            return 0;
+        while (currentRefill < allStop.length){
+            int lastRefill = currentRefill;
+            while (currentRefill < allStop.length - 1 && allStop[currentRefill + 1] - allStop[lastRefill] <= tank)
+                currentRefill++;
+            if (currentRefill == lastRefill)
+                return -1;
+            if (currentRefill == allStop.length - 1)
+                break;
+            if (currentRefill < allStop.length)
+                numRefill++;
+        }
+        return numRefill;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dist = sc.nextInt();
+        int tank = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(computeMinRefills(dist, tank, arr));
+    }
+}
