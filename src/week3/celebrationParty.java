37b7ccff971c4ba4c8ca8e2a75f9096f167e564c
Manthanoice <mvbosamiya20@gmail.com>
2021-09-16 22:51:58 +0530

    add party

---
+++ src/week3/celebrationParty.java
@@ -0,0 +1,33 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class celebrationParty {
+    static int childrenAge(float []arr) {
+        Arrays.sort(arr);
+        int lastChild = arr.length;
+        int currentChild = 1;
+        int startingPoint = 0;
+        int group = 0;
+        while(currentChild < lastChild){
+            if(arr[currentChild] - arr[startingPoint] > 1){
+                startingPoint = currentChild;
+                group++;
+            }
+            if(currentChild == lastChild - 1)
+                group++;
+            currentChild++;
+        }
+        return group;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float []arr = new float[n];
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextFloat();
+        System.out.println("Total groups would be: "+childrenAge(arr));
+    }
+}
