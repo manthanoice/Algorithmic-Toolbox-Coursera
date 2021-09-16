f61619c984034260e166ed58df7c0dd63fefcabd
Manthanoice <mvbosamiya20@gmail.com>
2021-09-16 17:11:09 +0530

    change

--- src/week3/carFueling.java
+++ src/week3/carFueling.java
@@ -3,13 +3,13 @@ package week3;
 import java.util.Scanner;
 
 public class carFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
+    static int computeMinRefills(int distance, int tank, int[] stops) {
         int currentRefill = 0;
         int numRefill = 0;
         int[] allStop = new int[stops.length + 2];
         System.arraycopy(stops, 0, allStop, 1, stops.length);
-        allStop[stops.length + 1] = dist;
-        if (tank > dist)
+        allStop[stops.length + 1] = distance;
+        if (tank > distance)
             return 0;
         while (currentRefill < allStop.length){
             int lastRefill = currentRefill;
@@ -26,13 +26,13 @@ public class carFueling {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int dist = sc.nextInt();
+        int distance = sc.nextInt();
         int tank = sc.nextInt();
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println(computeMinRefills(dist, tank, arr));
+        System.out.println(computeMinRefills(distance, tank, arr));
     }
 }
