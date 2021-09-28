c9afec0b82344d21e7eea260050e0b1fa5e916dd
Manthanoice <mvbosamiya20@gmail.com>
2021-09-28 16:22:49 +0530

    add mulPolNaive.java

---
+++ src/week4/mulPolNaive.java
@@ -0,0 +1,26 @@
+package week4;
+
+import java.util.Scanner;
+
+public class mulPolNaive {
+    static void pol(int []a, int []b, int length){
+        int n = (2*length)-1;
+        int []answer = new int[n];
+        for(int i=0; i<length; i++)
+            for(int j=0; j<length; j++)
+                answer[i+j]+=a[i]*b[j];
+        for(int i=0; i<n; i++)
+            System.out.print(answer[i]+" ");
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+        int [] b = new int[n];
+        for(int i=0; i<n; i++)
+            a[i] = sc.nextInt();
+        for(int i=0; i<n; i++)
+            b[i] = sc.nextInt();
+        pol(a,b,n);
+    }
+}
