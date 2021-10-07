b226f79a16ca817513fab217cce881893dd655da
Manthanoice <mvbosamiya20@gmail.com>
2021-10-07 17:16:21 +0530

    parentheses mistake

--- src/week4/insertionSort.java
+++ src/week4/insertionSort.java
@@ -7,17 +7,15 @@ public class insertionSort {
         int n = arr.length;
         for(int i=0; i<n; i++){
             int min_index = i;
-            for(int j=i+1; j<n; j++){
+            for(int j=i+1; j<n; j++)
                 if (arr[j]<arr[min_index])
                     min_index = j;
                 int temp = arr[min_index];
                 arr[min_index] = arr[i];
                 arr[i] = temp;
-            }
         }
         for (int j : arr) System.out.print(j + " ");
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
