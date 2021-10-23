44eaa310f1d6d0ff60647255a8017febefe699d4
[name] <69193208+manthanoice@users.noreply.github.com>
2021-10-23 16:30:46 +0530

    small edit :P

--- src/week5/coinChangeGreedy.java
+++ src/week5/coinChangeGreedy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class coinChangeGreedy {
     static int changeByGreedy(int money){
-        int []arr = {1,3,4};
+        int []arr = {4,3,1};
         int count = 0;
         for(int i=0; money!=0; i++){
             int x = money/arr[i];
